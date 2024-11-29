85b108abc6fe81b6b23814f57a59ec79049a95f5
volga <volga@DESKTOP-89UV4JU>
2024-11-29 16:43:36 +0500

    2

--- hw281124/Program.cs
+++ hw281124/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,22 +15,99 @@ namespace hw281124
         public static Random _rnd = new Random();
         static void Main(string[] args)
         {
-            /*
+                        /*
                         Пользователь запускает приложение и перед ним находится меню, в котором он может выбрать,
                         к какому вольеру подойти.При приближении к вольеру, пользователю выводится информация о том, что это за вольер, сколько
                         животных там обитает, их пол и какой звук издает животное.
                         Вольеров в зоопарке может быть много, в решении нужно создать минимум 4 вольера.
-                        */
+                        */            
+            ExecutionProgram program = new ExecutionProgram();
 
-            Valliere zoo = new Valliere();
+            program.StartProgram();
 
-            zoo.CreateZoo();
-            zoo.ShowZoo();
         }
     }
 
 
-    class Valliere
+    class ExecutionProgram
+    {
+        PaddockOne one = new PaddockOne();
+        PaddockTwo two = new PaddockTwo();
+        PaddockTree tree = new PaddockTree();
+        PaddockFour four = new PaddockFour();
+        public void StartProgram()
+        {
+            one.CreateZoo();
+            two.CreateZoo();
+            tree.CreateZoo();
+            four.CreateZoo();
+
+            string logOut = "exit";
+            string strCoice = "";
+            int numChoice = 0;
+
+            Console.WriteLine("\n\n\n\t\tВы подходите ближе и видите четыре вольера...\n\t\tПожалуйста подождите");
+            System.Threading.Thread.Sleep(5000);
+            Console.Clear();
+            while (strCoice.ToLower() != logOut) 
+            { 
+            Drow(ref strCoice, ref logOut, ref numChoice);
+            Coice(ref numChoice);
+
+            System.Threading.Thread.Sleep(1500);
+
+            Console.Clear() ;              
+            }
+        }
+
+        private void Drow(ref string str1, ref string str2, ref int num)
+        {
+            do
+            {
+                Console.Write("\n\n\n\t\tСделайте выбор:\n\t\t1: Подойти к первому вольеру\n\t\t2: Подойти к второму вольеру\n\t\t3: Подойти к третьему вольеру\n\t\t4: Подойти к четвертому вольеру\n\t\tВаш выбор -> ");
+
+                str1 = Console.ReadLine();
+
+                Console.Clear();
+            }
+            while (!int.TryParse(str1, out num) && str2 != str1.ToLower());
+        }
+
+        private void Coice(ref int num)
+        {
+            switch(num)
+            {
+                case 1:
+                    Console.Clear();
+                    one.ShowZoo();
+                   
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    two.ShowZoo();
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    tree.ShowZoo();
+                    break;
+
+                case 4:
+                    Console.Clear();
+                    four.ShowZoo();
+                    break;
+
+                    default:
+                    Console.Clear();
+                    //Console.WriteLine("\n\n\n\t\tОшибка ввода!!!");
+                    break;
+
+            }
+        }
+    }
+
+    abstract class Valliere
     {
         public List<Animals> _zoo = new List<Animals>();
 
@@ -57,7 +136,7 @@ namespace hw281124
                 num--;
             }
         }
-
+       
         public void ShowZoo()
         {
             Console.WriteLine($"Животных в этом вольере {_zoo.Count}"); 
@@ -66,7 +145,7 @@ namespace hw281124
             {
                 it.ActionAnimal();
 
-                System.Threading.Thread.Sleep(300);
+                System.Threading.Thread.Sleep(190);
             }
         }
 
@@ -151,4 +230,23 @@ namespace hw281124
         }
     }
 
+    class PaddockOne : Valliere
+    {
+
+    }
+
+    class PaddockTwo : Valliere
+    {
+
+    }
+
+    class PaddockTree : Valliere
+    {
+
+    }
+
+    class PaddockFour : Valliere
+    {
+
+    }
 }
