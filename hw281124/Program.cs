933d705f82c72bf4a42c03557b14451d98a85e1f
volga <volga@DESKTOP-89UV4JU>
2024-11-29 09:00:41 +0500

    Добавьте файлы проекта.

---
+++ hw281124/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw281124
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                        Пользователь запускает приложение и перед ним находится меню, в котором он может выбрать,
+                        к какому вольеру подойти.При приближении к вольеру, пользователю выводится информация о том, что это за вольер, сколько
+                        животных там обитает, их пол и какой звук издает животное.
+                        Вольеров в зоопарке может быть много, в решении нужно создать минимум 4 вольера.
+                        */
+
+            Valliere zoo = new Valliere();
+
+            zoo.CreateZoo();
+            zoo.ShowZoo();
+        }
+    }
+
+
+    class Valliere
+    {
+        public List<Animals> _zoo = new List<Animals>();
+
+        public void CreateZoo() 
+        {
+            Horse horse = new Horse();
+            Rabbit rabbit = new Rabbit();
+            Pig pig = new Pig();
+
+            _zoo.Add(horse);
+            _zoo.Add(rabbit);
+            _zoo.Add(pig);
+        }
+
+        public void ShowZoo()
+        {
+            foreach(Animals it in _zoo)
+            {
+                it.ActionAnimal();
+            }
+        }
+
+    }
+    abstract class Animals
+    {
+        public string _name { get; private set; }
+
+        public Animals(string name)
+        {
+            _name = name;
+        }
+
+        public virtual void ActionAnimal()
+        {
+            Console.WriteLine($"{_name} что-то делает!!! переписать в наследуемых классах");
+        }
+    }
+
+    class Horse : Animals
+    {
+        public Horse() : base("Лошадь") { }
+        
+        public override void ActionAnimal()
+        {
+            Console.WriteLine($"{_name} фыркает и ржет!!!");
+        }
+    }
+
+    class Rabbit : Animals
+    {
+        public Rabbit() : base("Кролик") { }
+
+        public override void ActionAnimal()
+        {
+            Console.WriteLine($"{_name} топает!!!");
+        }
+    }
+
+    class Pig : Animals
+    {
+        public Pig() : base("Свинья") { }
+
+        public override void ActionAnimal()
+        {
+            Console.WriteLine($"{_name} хрюкает!!!");
+        }
+    }
+
+}
---
+++ hw281124/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("hw281124")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hw281124")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("28ebc48e-560c-4db0-bc0a-2e23f2653a31")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
